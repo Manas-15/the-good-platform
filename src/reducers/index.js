7465c74a5aa87c01ec75c7c31d9f279b3357aaac
Ansuman Taria <ansuman.taria@andolasoft.co.in>
2022-04-13 18:09:52 +0530

    Pretiffier the codes

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,17 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import { authentication } from './authentication.reducer';
-import { registration } from './registration.reducer';
-import { users } from './users.reducer';
-import { corporates } from './corporates.reducer';
-import { alert } from './alert.reducer';
+import { authentication } from "./authentication.reducer";
+import { registration } from "./registration.reducer";
+import { users } from "./users.reducer";
+import { corporates } from "./corporates.reducer";
+import { alert } from "./alert.reducer";
 
 const rootReducer = combineReducers({
-    authentication,
-    registration,
-    users,
-    corporates,
-    alert
+  authentication,
+  registration,
+  users,
+  corporates,
+  alert,
 });
 
 export default rootReducer;
