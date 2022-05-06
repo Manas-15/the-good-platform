3a78924cd861cefdfbca1bd5335595463f688ca3
Ansuman Taria <ansuman.taria@andolasoft.co.in>
2022-05-06 11:20:25 +0530

    Fixed the data rendering issue

--- src/components/TransactionsHistory/ListTransactionsHistory.js
+++ src/components/TransactionsHistory/ListTransactionsHistory.js
@@ -5,6 +5,7 @@ import { transactionsHistoryActions } from "../../actions";
 import Loader from "../Shared/Loader";
 import * as moment from "moment";
 import { paymentConstants } from "../../constants";
+import { history } from "./../../helpers";
 
 const actionInitialValues = {
   userId: "",
@@ -17,7 +18,7 @@ const paymentStatusOption = [
   { label: "Failed", value: paymentConstants.PAYMENT_FAILURE },
 ];
 const ListTransactionsHistory = (props) => {
-  let history = useHistory();
+  // let history = useHistory();
   const [records, setRecords] = useState([]);
   const transactions = useSelector((state) => state.transactionsHistory);
   const charityPrograms = useSelector((state) => state.charityPrograms);
@@ -27,26 +28,23 @@ const ListTransactionsHistory = (props) => {
   const [actionContent, setActionContent] = useState("");
   const [actionType, setActionType] = useState("");
   const [selectedEmployee, setSelectedEmployee] = useState(Object);
+  const [currentPath, setCurrentPath] = useState(history.location.pathname);
   const dispatch = useDispatch();
   const employeeId = props?.match?.params?.employeeId;
-  console.log("eeeeeeeeeeeeeeeeeeeeeee", records);
   useEffect(() => {
-    console.log("eeeeeeeeeee inside eeeeeeeeeeee", records);
     dispatch(
       transactionsHistoryActions.getTransactionsHistory({
         employeeId: employeeId ? employeeId : null,
       })
     );
-
     charityPrograms?.items?.sponser?.forEach((e) => {
       charityProgramsOption.push({ label: e.soicalName, value: e.soicalId });
     });
     charityPrograms?.items?.other?.forEach((e) => {
       charityProgramsOption.push({ label: e.soicalName, value: e.soicalId });
     });
-  }, []);
+  }, [props]);
   useEffect(() => {
-    console.log("eeeeeeeeeee inside 3333 eeeeeeeeeeee", records);
     setRecords(transactions?.items);
   }, [transactions?.items]);
   const handleOpen = (action, item) => {
@@ -85,10 +83,10 @@ const ListTransactionsHistory = (props) => {
     );
   };
   return (
-    <div>
+    <div className="customContainer">
       <div className="row mt-3">
         <div className="col-md-6">
-          <h4>Transactions History</h4>
+          <h1 className="ant-typography customHeading">Transactions History</h1>
         </div>
         <div className="col-md-6 text-right">
           <div className="row mb-4">
@@ -124,75 +122,125 @@ const ListTransactionsHistory = (props) => {
           </div>
         </div>
       </div>
+      <div className="ant-row searchContainer mt-3 py-4 px-4 align-center">
+        <div className="ant-col ant-col-24  searchContainer">
+          <div className="ant-col ant-col-8">
+            <div className="ant-input-affix-wrapper inputFilterInput">
+              <span className="ant-input-prefix">
+                <i className="bi bi-search"></i>
+                <input
+                  placeholder="Search by Program"
+                  class="ant-input-search"
+                  type="text"
+                  value=""
+                />
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
       {transactions.loading && <Loader />}
-      <table className="table table-striped">
-        <thead>
-          <tr className="table-active">
-            <th>Sl#</th>
-            {!employeeId && <th>Name</th>}
-            <th>Program</th>
-            <th>Organization</th>
-            {!employeeId && <th>Corporate</th>}
-            <th>Transaction ID</th>
-            <th>Donation</th>
-            <th>Payment Mode</th>
-            <th>Payment Status</th>
-            <th>Payment Date</th>
-            {employeeId && <th>Action</th>}
-          </tr>
-        </thead>
-        <tbody>
-          {records?.length > 0 ? (
-            records?.map((transaction, index) => (
-              <tr key={index + 1}>
-                <td>{index + 1}</td>
-                {!employeeId && <td>{transaction?.employeeName}</td>}
-                <td>{transaction?.charityName}</td>
-                <td>{transaction?.socialOrg}</td>
-                {!employeeId && <td>{transaction?.corporateName}</td>}
-                <td>{transaction?.transactionId}</td>
-                <td>{transaction?.amount}</td>
-                <td>
-                  {transaction?.paymentMethod &&
-                    transaction?.paymentMethod.replace(/_/g, " ")}
-                </td>
-                <td>
-                  {transaction?.paymentStatus ===
-                  paymentConstants.PAYMENT_SUCCESS ? (
-                    <span className="badge badge-success">Success</span>
-                  ) : (
-                    <span className="badge badge-danger">Failed</span>
-                  )}
-                </td>
-                <td>
-                  {transaction?.paymentDate &&
-                    transaction?.paymentDate !== "None" &&
-                    moment(transaction?.paymentDate).format("DD/MM/YY, h:mm A")}
-                </td>
-                {employeeId && (
-                  <td>
-                    {transaction?.paymentStatus ===
-                      paymentConstants.PAYMENT_FAILURE && (
-                      <Link
-                        className="text-decoration-underline"
-                        onClick={() => downlad(transaction?.transactionId)}
-                      >
-                        Download 80G
-                      </Link>
+      <div className="ant-row">
+        <div className="ant-col ant-col-24 mt-2">
+          <div className="ant-table-wrapper">
+            <div className="ant-table">
+              <table>
+                <thead className="ant-table-thead">
+                  <tr>
+                    <th className="ant-table-cell">SR No.</th>
+                    {!employeeId && <th className="ant-table-cell">Name</th>}
+                    <th className="ant-table-cell">Program</th>
+                    <th className="ant-table-cell">Organization</th>
+                    {!employeeId && (
+                      <th className="ant-table-cell">Corporate</th>
                     )}
-                  </td>
-                )}
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan="10" className="text-center">
-                No transactions found
-              </td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+                    <th className="ant-table-cell">Transaction ID</th>
+                    <th className="ant-table-cell">Donation</th>
+                    <th className="ant-table-cell">Payment Mode</th>
+                    <th className="ant-table-cell">Payment Status</th>
+                    <th className="ant-table-cell">Payment Date</th>
+                    {employeeId && <th className="ant-table-cell">Action</th>}
+                  </tr>
+                </thead>
+                <tbody className="ant-table-tbody">
+                  {records?.length > 0 ? (
+                    records?.map((transaction, index) => (
+                      <tr
+                        key={index + 1}
+                        className="ant-table-row ant-table-row-level-0"
+                      >
+                        <td className="ant-table-cell">{index + 1}</td>
+                        {!employeeId && (
+                          <td className="ant-table-cell">
+                            <span className="ant-typography font-weight-bold">{transaction?.employeeName}</span>
+                          </td>
+                        )}
+                        <td className="ant-table-cell">
+                        <span className="ant-typography font-weight-bold">{transaction?.charityName}</span>
+                        </td>
+                        <td className="ant-table-cell">
+                        <span className="ant-typography font-weight-bold">{transaction?.socialOrg}</span>
+                        </td>
+                        {!employeeId && (
+                          <td className="ant-table-cell">
+                            {transaction?.corporateName}
+                          </td>
+                        )}
+                        <td className="ant-table-cell">
+                          {transaction?.transactionId}
+                        </td>
+                        <td className="ant-table-cell">
+                          {transaction?.amount}
+                        </td>
+                        <td className="ant-table-cell">
+                          {transaction?.paymentMethod &&
+                            transaction?.paymentMethod.replace(/_/g, " ")}
+                        </td>
+                        <td className="ant-table-cell text-uppercase">
+                          {transaction?.paymentStatus ===
+                          paymentConstants.PAYMENT_SUCCESS ? (
+                            <span className="text-success">Success</span>
+                          ) : (
+                            <span className="text-danger">Failed</span>
+                          )}
+                        </td>
+                        <td className="ant-table-cell">
+                          {transaction?.paymentDate &&
+                            transaction?.paymentDate !== "None" &&
+                            moment(transaction?.paymentDate).format(
+                              "DD/MM/YY, h:mm A"
+                            )}
+                        </td>
+                        {employeeId && (
+                          <td className="ant-table-cell">
+                            {transaction?.paymentStatus ===
+                              paymentConstants.PAYMENT_SUCCESS && (
+                              <Link
+                                className="text-decoration-underline"
+                                onClick={() =>
+                                  downlad(transaction?.transactionId)
+                                }
+                              >
+                                Download 80G
+                              </Link>
+                            )}
+                          </td>
+                        )}
+                      </tr>
+                    ))
+                  ) : (
+                    <tr>
+                      <td colSpan="10" className="text-center">
+                        No transactions found
+                      </td>
+                    </tr>
+                  )}
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
       {/* <div className="row mb-4">
         <div className="col-md-6">
           <p>Showing 1 to 10 of 20 records</p>
