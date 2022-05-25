c9f64e9f100d7ae89786cbd16c53ad79f6a0e8c3
Ansuman Taria <ansuman.taria@andolasoft.co.in>
2022-05-25 18:13:07 +0530

    Added terms and privacy

---
+++ src/components/PrivacyPolicy/PrivacyPolicy.js
@@ -0,0 +1,51 @@
+import React from "react";
+
+const PrivacyPolicy = (props) => {
+  return (
+    <div className="row align-items-center authFormMargin">
+      <div className="col-md-4">
+        <div className="authForm setPasswordLeftContent">
+          <h1 className="textHeading">Welcome back to The Good Platform!</h1>
+          <img height="350" src="/assets/img/smartphone2.png" />
+        </div>
+      </div>
+      <div className="col-md-8 mt-3">
+        <div className="registrationContent">
+          <h2 className="text-center mb-4">Privacy Policy</h2>
+          <p>
+            <strong>Where does it come from?</strong>
+          </p>
+          <p>
+            Contrary to popular belief, Lorem Ipsum is not simply random text.
+            It has roots in a piece of classical Latin literature from 45 BC,
+            making it over 2000 years old. Richard McClintock, a Latin professor
+            at Hampden-Sydney College in Virginia, looked up one of the more
+            obscure Latin words, consectetur, from a Lorem Ipsum passage, and
+            going through the cites of the word in classical literature,
+            discovered the undoubtable source. Lorem Ipsum comes from sections
+            1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes
+            of Good and Evil) by Cicero, written in 45 BC. This book is a
+            treatise on the theory of ethics, very popular during the
+            Renaissance. The first line of Lorem Ipsum.
+          </p>
+          <p>
+            <strong>Where can I get some?</strong>
+          </p>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but
+            the majority have suffered alteration in some form, by injected
+            humour, or randomised words which don't look even slightly
+            believable. If you are going to use a passage of Lorem Ipsum, you
+            need to be sure there isn't anything embarrassing hidden in the
+            middle of text. All the Lorem Ipsum generators on the Internet tend
+            to repeat predefined chunks as necessary, making this the first true
+            generator on the Internet. It uses a dictionary of over 200 Latin
+            words, combined with a handful of model sentence structures, to
+            generate Lorem Ipsum which looks reasonable.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PrivacyPolicy;
---
+++ src/components/socialOrganizations/SocialOrganizationsPortal.js
@@ -0,0 +1,86 @@
+import React, { useEffect, useState, createRef } from "react";
+import { useHistory } from "react-router-dom";
+import { socialOrganizationActions, selectedOrganizationActions } from "../../actions";
+import { useDispatch, useSelector } from "react-redux";
+import Loader from "../Shared/Loader";
+import "./../../assets/css/corporates.scss";
+import { Link } from "react-router-dom";
+// import corporates from "./../../config/corporates.json";
+const actionInitialValues = {
+  userId: "",
+  requestType: "",
+};
+const SocialOrganizationsPortal = () => {
+  let history = useHistory();
+  const socialOrganizations = useSelector((state) => state.socialOrganizations);
+  const user = useSelector((state) => state.employee.user);
+  const dispatch = useDispatch();
+  const [open, setOpen] = useState(false);
+  const logout = () => {
+    // localStorage.removeItem("user");
+    localStorage.clear();
+    history.push("/");
+  };
+  if (socialOrganizations.loading) {
+    document.getElementById("root").classList.add("loading");
+  } else {
+    document.getElementById("root").classList.remove("loading");
+  }
+  useEffect(() => {
+    dispatch(socialOrganizationActions.getSocialOrganizations());
+  }, []);
+  const setSocialOrganization = (organizationId) => {
+    console.log(">>>>>>>>>>>>>>>>> organizationId", organizationId);
+    dispatch(selectedOrganizationActions.selectedOrganization(organizationId));
+  };
+  return (
+    <div>
+      <div className="row mb-4">
+        <div className="col-md-4 text-center offset-md-3">
+          <a href="/">
+            <img
+              src="/assets/img/logo.png"
+              alt="The Good Platform Logo"
+              height={30}
+            />
+          </a>
+        </div>
+      </div>
+      <div className="row mb-4">
+        <div className="col-md-4 text-center offset-md-3">
+          <h4>Social Organizations Portal</h4>
+        </div>
+      </div>
+      <div className="row mb-4">
+        <div className="col-md-4 offset-md-3">
+          {socialOrganizations.loading && <Loader />}
+          {socialOrganizations?.items && socialOrganizations?.items.length > 0 ? (
+            <div className="card corporates-lunchpad">
+              <ul className="pl-0">
+                {socialOrganizations?.items.map((org, index) => {
+                  return (
+                    <li key={index + 1}>
+                      <Link
+                        to={`/organizations/${org.id}/payroll-batch`}
+                      >
+                        {org?.name}
+                      </Link>
+                    </li>
+                  );
+                })}
+                <li key={"logout"} className="logout">
+                  <Link onClick={logout}>Logout</Link>
+                </li>
+              </ul>
+            </div>
+          ) : (
+            <div className="text-center">
+              <strong>No social organizations found</strong>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+export default SocialOrganizationsPortal;
