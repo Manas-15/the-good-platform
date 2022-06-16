bfc47ee8c6a69f5ed0fefa1f9aa57728d8a0ab93
Ansuman Taria <ansuman.taria@andolasoft.co.in>
2022-06-16 13:14:56 +0530

    Changes for default corporate in signup

--- src/components/employee/EmployeeForm.js
+++ src/components/employee/EmployeeForm.js
@@ -22,7 +22,7 @@ const initialValues = {
   email: "",
   employeeId: "",
   pan: "",
-  corporateProfileId: 1,
+  corporateProfileId: 3,
   organizationJoiningDate: "",
   gender: "",
   contactNumber: "",
