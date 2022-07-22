e12139389b0abadff2ffa8a1815adbca808b3a7d
Manas-Dweb <manaspasayat3@gmail.com>
2022-07-22 16:18:43 +0530

    employee portal

--- src/components/charityPrograms/CharityPrograms.js
+++ src/components/charityPrograms/CharityPrograms.js
@@ -277,8 +277,8 @@ const CharityPrograms = (props) => {
         </div>
       </div>
       <div className="ant-tabs-nav-wrap">
-        {currentPortal?.currentView ===
-        ("Individual Portal" || "Others Portal") ? (
+        {currentPortal?.currentView === "Individual Portal" ||
+        currentPortal?.currentView === "Others Portal" ? (
           <>
             {currentView === charityProgramConstants.LIST_VIEW && (
               <ListCharityPrograms
--- src/components/socialOrganizations/SocialOrganizations.js
+++ src/components/socialOrganizations/SocialOrganizations.js
@@ -6,7 +6,7 @@ import {
   paginationConstants,
   charityProgramConstants,
   viewPortalConstants,
-  userConstants
+  userConstants,
 } from "../../constants";
 import { socialOrganizationActions } from "../../actions";
 import { Tabs } from "antd";
@@ -44,6 +44,7 @@ const SocialOrganizations = () => {
   const isOthersPortal =
     currentPortal?.currentView === viewPortalConstants.OTHERS_PORTAL;
 
+  console.log(isIndividualPortal, isOthersPortal);
   // Pagination
   const [totalCount, setTotalCount] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
@@ -65,20 +66,20 @@ const SocialOrganizations = () => {
               individualId:
                 loggedInUserType === userConstants.INDIVIDUAL
                   ? user?.uuid
-                  : null
+                  : null,
             }
           : isOthersPortal
           ? {
-              loggedInUserType: loggedInUserType
+              loggedInUserType: loggedInUserType,
             }
           : {
               pageNumber: currentPage.toString(),
-              corporateId: selectedCorporate?.corporate?.id,
+              corporateId: user?.corporateId,
               employeeId: isEmployeePortal ? user?.emp_id : null,
               individualId: user?.uuid,
               loggedInUserType: loggedInUserType,
               pageSize: pageSize.toString(),
-              userId: user?.user_id
+              userId: user?.user_id,
             }
       )
     );
