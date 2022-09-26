4fe76b16d6f1aef757eaef1b78bbd4da656fe4ea
Ansuman Taria <ansuman.taria@andolasoft.co.in>
2022-09-26 15:34:24 +0530

    Changes for socail organizations

--- src/reducers/socialOrganizations.reducer.js
+++ src/reducers/socialOrganizations.reducer.js
@@ -8,9 +8,13 @@ export function socialOrganizations(state = {}, action) {
         loading: true,
         userType: action?.data?.loggedInUserType,
         userRole: action?.data?.userRole,
-        individualId: action?.data?.individualId,
+        individualId: action?.data?.individualId
       };
     case socialOrganizationConstants.GET_SOCIAL_ORGANIZATIONS_SUCCESS:
+      console.log("11111", action?.socialOrganizations?.data);
+      console.log("44444", state?.individualId);
+      console.log("3333", viewPortalConstants.PAYMENT_ADMIN);
+      console.log("555555", state?.userRole);
       if (
         (state?.userType === 2 || state?.userType === 3) &&
         state?.userRole !== viewPortalConstants.PAYMENT_ADMIN &&
@@ -19,19 +23,21 @@ export function socialOrganizations(state = {}, action) {
         return {
           items: action?.socialOrganizations?.data?.social_organization,
           totalCount: action?.socialOrganizations?.data?.data?.numberOfElements,
-          loading: false,
+          loading: false
         };
       } else {
         return {
-          items: action?.socialOrganizations?.data?.data,
+          items: action?.socialOrganizations?.data?.data
+            ? action?.socialOrganizations?.data?.data
+            : action?.socialOrganizations?.data?.social_organization,
           totalCount: action?.socialOrganizations?.data?.totalCount,
-          loading: false,
+          loading: false
         };
       }
     case socialOrganizationConstants.GET_SOCIAL_ORGANIZATIONS_FAILURE:
       return {
         error: action.error,
-        loading: false,
+        loading: false
       };
     default:
       return state;
